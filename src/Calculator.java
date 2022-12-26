f54f6ac316a0d6a011a7078b61d050e23a167f50
shubhamk <shubhamk@opentext.com>
2022-12-26 15:07:30 +0530

    Added add method

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static void main(String[] args){
+        int a = 5, b = 5;
+        System.out.println(add(a,b));
+    }
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+}

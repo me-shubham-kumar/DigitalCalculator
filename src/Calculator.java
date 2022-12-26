e1e01c4cc948a54b210a4dd6a807e9136ef9f63a
shubhamk <shubhamk@opentext.com>
2022-12-26 15:20:44 +0530

    Added multiplication method

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,9 +2,15 @@ public class Calculator {
     public static void main(String[] args){
         int a = 5, b = 5;
         System.out.println(add(a,b));
+        System.out.println(multiplication(a,b));
+
     }
 
     public static int add(int a, int b){
         return a+b;
+    }
+
+    public static int multiplication(int a, int b){
+        return a*b;
     }
 }

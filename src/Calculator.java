8334a555d87edb113d4a1b220740f6e023fea13b
shubhamk <shubhamk@opentext.com>
2022-12-26 15:24:28 +0530

    Added division method

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,9 +2,18 @@ public class Calculator {
     public static void main(String[] args){
         int a = 5, b = 5;
         System.out.println(add(a,b));
+        System.out.println(div(a,b));
     }
 
     public static int add(int a, int b){
         return a+b;
+    }
+
+    public static float div(int a, int b){
+        if(b != 0){
+            return a/b;
+        }else{
+            return 0;
+        }
     }
 }
